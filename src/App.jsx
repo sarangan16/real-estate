c3ff885b5a92d3a5ae563b1e0c7bd4cc534e3346
sarangan16 <mailsarangan@gmail.com>
2025-08-14 13:22:26 +0200

    projects sameple added, carousel created for projects, testimonial and contact components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
+import Projects from "./components/Projects";
+import Testimonial from "./components/Testimonial";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
       <Header />
       <About />
+      <Projects />
+      <Testimonial />
+      <Contact />
     </div>
   );
 };
---
+++ src/components/Contact.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "74be6621-bf98-4fb5-a3f2-70fe2e0373ca");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      alert("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      alert(data.message);
+      setResult("");
+    }
+  };
+
+  return (
+    <div
+      className="text-center p-6 py-20 lg:px-32 overflow-hidden"
+      id="Contact"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Contact{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          With Us
+        </span>
+      </h1>
+      <p className="text-gray-500 max-w-80 text-center mb-8 mx-auto">
+        Crafting Spaces, Building Legacies-Explore Our Portfolio
+      </p>
+
+      <form
+        onSubmit={onSubmit}
+        className="max-w-2xl mx-auto text-gray-600 pt-8"
+      >
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/2 text-left">
+            Your Name
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4"
+              type="text"
+              name="Name"
+              placeholder="Your Name"
+              required
+            />
+          </div>
+          <div className="w-full md:w-1/2 text-left md:pl-4">
+            Your Email
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4"
+              type="email"
+              name="Email"
+              placeholder="Your Email"
+              required
+            />
+          </div>
+        </div>
+        <div className="my-6 text-left">
+          Message
+          <textarea
+            className="w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none"
+            name="Message"
+            placeholder="Message"
+            required
+          ></textarea>
+        </div>
+        <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded">
+          {result ? result : "Send Message"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
